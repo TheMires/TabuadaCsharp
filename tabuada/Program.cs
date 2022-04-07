e15a9f97280479adb9e0c28346719a10896b08de
TheMires <thamiresfreitas051@gmail.com>
2022-04-06 22:08:17 -0300

    @TheMires TabuadaCsharp

---
+++ tabuada/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Mallu, digite um numero para multiplicar: ");
+            numero = Convert.ToInt32 (Console.ReadLine());
+
+            for(int x = 1; x <= 10; x++)
+            {
+                int resultado = numero * x;
+                    Console.WriteLine($"{numero} X {x} = {resultado}");
+               
+            }
+
+        }
+    }
+}
